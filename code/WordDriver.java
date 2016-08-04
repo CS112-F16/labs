a146409daebaa4d0933ef8bc61364c0d98122fc8
Sami Rollins <srollins@cs.usfca.edu>
2016-08-04 14:26:06 -0700

    adding code for lab 3

---
+++ code/WordDriver.java
@@ -0,0 +1,45 @@
+/**
+	A program to test the Word class for lab assignment 3.
+
+	@author Sami Rollins	
+ **/
+public class WordDriver {
+
+	/**
+		Takes as input a String, creates an object of type Word, and 
+		invokes the methods of the Word object.
+	 **/
+	public static void testWord(String wordStr) {		
+		Word word = new Word(wordStr);
+
+		System.out.println("Word: " + wordStr);
+
+		System.out.println("\tOccurrences of \'a\': " + word.countChars('a'));
+		System.out.println("\tOccurrences of \'i\': " + word.countChars('i'));
+		System.out.println("\tOccurrences of \'s\': " + word.countChars('s'));
+		System.out.println("\tOccurrences of \'c\': " + word.countChars('c'));
+
+		System.out.println(); //print a blank line
+
+		String toMatch = "computer";
+		System.out.println("\tMatching characters in \'" + toMatch + "\': " + word.matchingChars(toMatch));		
+		
+		toMatch = "spin";
+		System.out.println("\tMatching characters in \'" + toMatch + "\': " + word.matchingChars(toMatch));		
+
+		toMatch = "ample";
+		System.out.println("\tMatching characters in \'" + toMatch + "\': " + word.matchingChars(toMatch));		
+
+		System.out.println("\tIs " + wordStr + " a palindrome?: " + word.isPalindrome());		
+	}
+
+	/**
+		Main logic of the test program.
+	**/
+	public static void main(String[] args) {
+		testWord("science");
+		System.out.println(); //print a blank line
+		testWord("Civic");
+	}
+
+}
